ef85eba4f1dac329338db3e425d51e48ef8df192
victor-dat21b <vict702k@stud.kea.dk>
2022-12-07 12:13:33 +0100

    Worked on reservations regarding multiple reservations

--- pages/reservationPage/reservationPage.js
+++ pages/reservationPage/reservationPage.js
@@ -10,8 +10,27 @@ let reserveButton
 
 let chairData
 
-let chairDataList
+let seatButton
+
+
 export function reservationPageSetup() {
+    
+
+    reserveButton = document.querySelector("#reserveButton")
+
+    seatButton = document.querySelector("#seatAmount")
+
+    const jsonBody = localStorage.getItem("accessToken")
+    
+
+    reserveButton.addEventListener("click", createReservation)
+
+    seatButton.addEventListener("click", amountOfSeats)
+    
+
+}
+
+function segmentAndChairSetup(){
     segmentDropdown = document.querySelector("#segmentDropdown")
 
     chairDropdown = document.querySelector("#chairDropdown")
@@ -20,19 +39,16 @@ export function reservationPageSetup() {
 
     chairChoice = document.querySelector("#chairChoice")
 
-    reserveButton = document.querySelector("#reserveButton")
-
-    const jsonBody = localStorage.getItem("accessToken")
-
-    segmentDropdownFunction()
-
     segmentDropdown.addEventListener("click", segmentChosen)
 
     chairDropdown.addEventListener("click", chairChosen)
 
-    reserveButton.addEventListener("click", createReservation)
-
+    segmentDropdownFunction()
+    
 }
+
+
+
 
 async function segmentDropdownFunction() {
 
@@ -118,6 +134,69 @@ async function createReservation() {
     }
 }
 
+async function amountOfSeats(event) {
+    document.getElementById("seat-holder").innerHTML = "";
+{event.target.text}
+let counter = event.target.text;
+
+let amountOfCards;
+for (let i = 0; i < counter; i++) {
+    const reserveCards = `
+                <div class="row">
+                <div class="col-sm-6">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">Vælg et segment</h5>
+                      <p class="card-text">Du skal vælge det segment du ønsker at sidde i.</p>
+                      <div class="dropdown" style="padding-right:20px ;">
+                        <button class="btn btn-danger dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Vælg Segment
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-dark" id="segmentDropdown${i+1}">
+                            
+                        </ul>
+                          
+                        <p id="segmentChoice" style="display:inline-flex"></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-sm-6">
+                  <div class="card">
+                    <div class="card-body">
+                      <h5 class="card-title">Vælg en plads</h5>
+                      <p class="card-text">Du skal vælge den plads du gerne vil sidde på.</p>
+                      <div class="dropdown">
+                        <button class="btn btn-danger dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Vælg Stol
+                        </button>
+                        <ul class="dropdown-menu dropdown-menu-dark" id="chairDropdown${i+1}">
+                            
+                        </ul>
+                          
+                        <p id="chairChoice" style="display:inline-flex"></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+                `
+
+    console.log(reserveCards)
+        document.getElementById("seat-holder").innerHTML += reserveCards;
+}
+    segmentAndChairSetup()
+                
+
+}
+
+
+
+
+
+
+
+
 function reservationMadePage() {
 
     window.router.navigate("/reservationMade")
@@ -131,3 +210,5 @@ function backToReservePage() {
     window.router.navigate("/reservation")
 
 }
+
+
